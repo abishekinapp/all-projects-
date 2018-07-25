0ee3c95559d942c1f59cdb28526c56e6eaf25103
Abishek <41431401+abishekinapp@users.noreply.github.com>
2018-07-25 17:29:07 +0530

    django 25-7-2018

--- abishek_Training/tutorials/accounts/urls.py
+++ abishek_Training/tutorials/accounts/urls.py
@@ -3,9 +3,11 @@
 
 from django.conf.urls import url
 from . import views  # the '.' means it will serch in the project folder and fid out the file
-
+from django.contrib.auth.views import login
 
 urlpatterns =[
     url(r'^$',views.home)
+    url(r'^ login/$',login,{'template_name':'account/login.htlm'})
 
 ]
+  
--- abishek_Training/tutorials/accounts/views.py
+++ abishek_Training/tutorials/accounts/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-        return render(request,'accounts/login.html')
+    name='abishek'
+    numbers = [1,3,4,6,3,2,4,5,55]
+
+    args={'myname':name , 'number':numbers}
+    return render(request,'accounts/home.html',args)
